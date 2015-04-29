ceda5fb5d663f352f884770c6a6a3abad41eb30c
wuqiany2 <wuqiany2@msu.edu>
2015-04-28 23:59:35 -0400

    draw line functionality
    Now you can change the pen size in the editor.
    Also if you hold left control and draw 2 dots at runtime, it will form a
    line segment.

--- Assets/DrawTool.cs
+++ Assets/DrawTool.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class DrawTool : MonoBehaviour
 {
     Material mat;
+    Color[] colors;
+    public int penSize = 10;
+    List<Vector2> points;
+    
 
     void Start()
     {
+        points = new List<Vector2>();
         mat = GetComponent<SpriteRenderer>().material;
+        colors = new Color[penSize*penSize];
+        for (int i = 0; i < penSize*penSize; i++) { colors[i] = Color.black; }
     }
 
     void Update()
@@ -20,13 +28,42 @@ public class DrawTool : MonoBehaviour
             {
                 //Debug.Log( hitLoc );
                 Texture2D tex = (Texture2D)mat.GetTexture( "_Casters" );
-
-                tex.SetPixel( (int)( tex.width * hitLoc.x ), (int)( tex.height * hitLoc.y ), Color.black );
+                tex.SetPixels( (int)( tex.width * hitLoc.x ), (int)( tex.height * hitLoc.y ), penSize,penSize, colors,0 );
                 tex.Apply();
                 mat.SetTexture( "_Casters", tex );
                 GetComponent<SpriteRenderer>().material = mat;
             }
         }
+
+        //method to draw straight line
+
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetMouseButtonDown(0))
+        { 
+            if (points.Count < 2)
+            {
+                Vector2 hitLoc = GetUVCoordinate();
+                if (hitLoc.x >= 0 && hitLoc.y >= 0)
+                {
+                    points.Add(hitLoc);
+                }
+            }
+        }
+
+        if (points.Count == 2)
+        {
+            Texture2D tex = (Texture2D)mat.GetTexture("_Casters");
+            int x0 = (int)(tex.width * points[0].x);
+            int y0 = (int)(tex.width * points[0].y);
+            int x1 = (int)(tex.width * points[1].x);
+            int y1 = (int)(tex.width * points[1].y);
+
+            DrawLine(tex, x0, y0, x1, y1);
+
+            points.Clear();
+        }
+
+        //finish line draw
+
     }
 
     private Vector2 GetUVCoordinate()
@@ -46,4 +83,55 @@ public class DrawTool : MonoBehaviour
             return new Vector2( -1.0f, -1.0f );
         }
     }
+
+
+    void DrawLine(Texture2D tex, int x0, int y0, int x1, int y1)
+    {
+        int dy = (int)(y1 - y0);
+        int dx = (int)(x1 - x0);
+        int stepx, stepy;
+
+        if (dy < 0) { dy = -dy; stepy = -1; }
+        else { stepy = 1; }
+        if (dx < 0) { dx = -dx; stepx = -1; }
+        else { stepx = 1; }
+        dy <<= 1;
+        dx <<= 1;
+
+        float fraction = 0;
+
+        tex.SetPixels(x0, y0, penSize, penSize, colors) ;
+        if (dx > dy)
+        {
+            fraction = dy - (dx >> 1);
+            while (Mathf.Abs(x0 - x1) > 1)
+            {
+                if (fraction >= 0)
+                {
+                    y0 += stepy;
+                    fraction -= dx;
+                }
+                x0 += stepx;
+                fraction += dy;
+                tex.SetPixels(x0, y0, penSize, penSize, colors);
+            }
+        }
+        else
+        {
+            fraction = dx - (dy >> 1);
+            while (Mathf.Abs(y0 - y1) > 1)
+            {
+                if (fraction >= 0)
+                {
+                    x0 += stepx;
+                    fraction -= dy;
+                }
+                y0 += stepy;
+                fraction += dx;
+                tex.SetPixels(x0, y0, penSize, penSize, colors);
+            }
+        }
+    }
+
+
 }
